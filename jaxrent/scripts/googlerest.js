5943134c198fd0519a89f417c1d4ed95e29a184c
Corey Jackson <corey.jackson@jax.tech>
2025-04-14 14:08:45 -0400

    supported languages test

--- jaxrent/scripts/googlerest.js
+++ jaxrent/scripts/googlerest.js
@@ -1,4 +1,4 @@
-function test()
+function translateTest()
 {
     const outputElement = document.getElementById('output');
 
@@ -13,6 +13,39 @@ function test()
         body: JSON.stringify({
             q: 'the house is built with wood',
             target: 'fr-CA'
+        })
+    };
+
+    fetch(apiUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                console.log("response.status=" + response.status + "response.statusText=" + response.statusText + "response.URL=" + response.URL);
+                throw new Error(`Network response was not ok, status code ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            outputElement.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+function displayLanguage()
+{
+    const outputElement = document.getElementById('divLanguageDisplay');
+
+    const apiUrl = 'POST https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyDAL-P6lY5iERPG63rGIls1ZEYlzGMOjbY';
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+            target: 'zh-TW'
         })
     };
 
