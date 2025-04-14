fe3e40dd7c44ebe3aa55cb2973ec3c0c0f400d40
Corey Jackson <corey.jackson@jax.tech>
2025-04-14 02:14:13 -0400

    updated content security policy

---
+++ jaxrent/scripts/googlerest.js
@@ -0,0 +1,35 @@
+function test()
+{
+    const outputElement = document.getElementById('output');
+
+    const apiKey = 'AIzaSyBZ74oUzbj7rjjrff-L5QKh2ereQbugRko';
+    const apiUrl = 'https://translation.googleapis.com/language/translate/v2?key=AIzaSyBZ74oUzbj7rjjrff-L5QKh2ereQbugRko';
+
+    const requestOptions = {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            /*'Authorization': 'Bearer AIzaSyBZ74oUzbj7rjjrff-L5QKh2ereQbugRko',*/
+            'x-goog-user-project': 'jaxrent',
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: {
+            'q': 'the house is built with wood',
+            'target': 'fr-CA'
+        },
+    };
+
+    fetch(apiUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            outputElement.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
